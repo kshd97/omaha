a7e62f9d0fad271c6116a74fd8012f96e6b538b6
Abhay Swaroop <abhaytyagi1998@gmail.com>
2018-07-10 12:52:32 +0300

    Instructions for rmr

--- config/connections.js
+++ config/connections.js
@@ -44,8 +44,8 @@ module.exports.connections = {
     adapter: 'sails-mysql',
     host: 'localhost',
     user: 'root', //optional
-    password: '1997', //optional
-    database: 'omaha' //optional
+    password: 'edwardkenway12', //optional
+    database: 'users' //optional
   },
 
   /***************************************************************************
